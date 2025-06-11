6947d7c4b1f50c8e0b2af4ebc2e24bdbd5a05051
TejaKanakala <satyatejakanakala@gmail.com>
2025-06-12 01:03:07 +0530

    adding a pr request

--- src/main/java/com/techeazy/devops/controller/TestController.java
+++ src/main/java/com/techeazy/devops/controller/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @GetMapping
-    public  String test() {
-        return "Successfully Deployed";
+    public  String testPR() {
+        return "testing pull request demo";
     }
 }
