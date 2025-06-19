6418dd75d7e85b0c4528444e46d28c4a6b93f93a
Teja Kanakala <satyatejakanakala@gmail.com>
2025-06-19 19:20:56 +0000

    🔥 Fix: Removed duplicate output block

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,7 +1,3 @@
-output "info" {
-  value = "✅ EC2 instance was manually created. Terraform is now only managing S3/IAM."
-}
-
 
 # output "ec2_public_ip" {
 #   value = aws_instance.dev_ec2.public_ip
